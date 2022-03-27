24a31e64a88b7b456255bc62eab494f81150632e
swvitaliy <sw.vitaliy@gmail.com>
2022-03-27 16:08:44 +0300

    update backup.sh; error handling

---
+++ backup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+
+PROGNAME="$(basename $0)"
+
+function error() {
+    JOB="$0"              # job name
+    LASTLINE="$1"         # line of error occurrence
+    LASTERR="$2"          # error code
+    echo "ERROR in ${JOB} : line ${LASTLINE} with exit code ${LASTERR}"
+  
+    finish=`date -Iseconds`
+    echo "${PROGNAME} finished with status ERROR (${finish})"
+    exit 1
+}
+trap 'error ${LINENO} ${?}' ERR
+
+#####
+
+start=`date -Iseconds`
+echo "${PROGNAME} starts (${start})"
+
+if mountpoint -q backups; then
+  echo "/home/vit/backups already mounted"
+else
+  echo "mount dir /home/vit/backups"
+  sshfs -o StrictHostKeyChecking=no  /home/vit/backups rock@100.109.20.101:///home/rock/backups
+
+  echo "dir /home/vit/backups mounted successful"
+fi
+
+rsync -av --exclude="node_modules" --exclude="bower_components" --no-links /home/vit/Projects /home/vit/backups
+
+finish=`date -Iseconds`
+echo "${PROGNAME} finished with status OK (${finish})"
+
+
