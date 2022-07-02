93d5b93d84def55d87e38ada1117ae86fe940d7c
Vitaliy <sw.vitaliy@gmail.com>
2022-07-02 12:49:25 +0300

    backup

--- backup.sh
+++ backup.sh
@@ -4,6 +4,10 @@ set -eu
 set -o pipefail  # trace ERR through pipes
 set -o errtrace  # trace ERR through 'time command' and other functions
 
+src="${1}"
+dst="${2}"
+mnt="${3}"
+
 function notify1() {
   sudo -u vit DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send "$1" "$2"
 }
@@ -37,18 +41,18 @@ fi
 
 start=`date -Iseconds`
 log1 "${PROGNAME} running..."
-notify1 "Backup" "Backup starts"
+notify1 "Backup" "Backup running..."
 
-if mountpoint -q /home/vit/backups; then
-  log1 "/home/vit/backups already mounted"
+if mountpoint -q "${dst}"; then
+  log1 "${dst} already mounted"
 else
-  log1 "mount dir /home/vit/backups"
-  sshfs -o StrictHostKeyChecking=no /home/vit/backups rockpi:///home/rock/backups
+  log1 "mount dir ${dst}"
+  sshfs -o StrictHostKeyChecking=no "${dst}" "${mnt}"
 
-  log1 "dir /home/vit/backups mounted successful"
+  log1 "dir ${dst} mounted successful"
 fi
 
-rsync -av --exclude="node_modules" --exclude="bower_components" --no-links /home/vit/Projects /home/vit/backups
+rsync -av --exclude="node_modules" --exclude="bower_components" --no-links "${src}" "${dst}"
 
 finish=`date -Iseconds`
 ddiff=$(dateutils.ddiff "${start}" "${finish}" -f '%Yy %dd %Hh %Mm %Ss' | sed 's/\b0[ymdh]\b\s*//g')
